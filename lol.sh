36fd3fd238d6bb2445fed3d963396086a3240eee
mizr1337 <40857009+mizr1337@users.noreply.github.com>
2018-11-05 16:02:27 +0700

    Add files via upload

---
+++ lol.sh
@@ -0,0 +1,81 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "Santet 0nline"
+sleep 1
+echo "\033[31;1m Macam Macam Tools Santet"
+sleep 1
+echo "\033[33;1m Author: MzrHx0r"
+sleep 1
+echo "\033[33;1m Thanks to Mr. Rm for referensi na :v"
+echo "\033[32;1m ★contack: jancokblog@gmail.com"
+sleep 1
+echo "\033[34;1m ★Teknik Komputer Dan Jaringan 1"
+sleep 1
+echo "\033[36;1m Santet 0nline ini hanya menggunakan nomor telepon korban"
+echo "\033[36;1m Macam macam santet 0nline :"
+echo "1. Memasukan Paku ke Ban Motor Korban"
+echo "2. Rante motor korban tiba tiba hilang"
+echo "3. Mengubah warna darah korban dari merah menjadi pink"
+echo "4. Request tools nya? Pm wae"
+echo "0. Keluar"
+echo "\033[30;1m81. Tools ieu jang seseruan we"
+echo "\033[33;1m Pilih Angka na:"
+read mzr
+if [ $mzr = 1 ] || [ $mzr = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "Bom Call Tokopedia"
+php 1.php
+fi
+
+if
+[ $mzr = 2 ] || [ $mzr = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Telkomsel"
+echo "\033[31;1m Tools ini mungkin Tidak aktif"
+php 2.php
+fi
+
+if [ $mzr = 3 ] || [ $mzr = 3 ]
+then
+clear
+echo "\033[31;1m"
+figlet "Mthr Mall"
+php 3.php
+fi
+
+if
+[ $mzr = 81 ] || [ $mzr = 81 ]
+then
+clear
+toilet -f slant --gay "Santet 0nline"
+echo "\033[31;1mNama tools: Santet 0nline"
+sleep 1
+echo "\033[33;1mKarya: MzrHx0r"
+sleep 1
+echo "\033[32;1mVersi: v1"
+sleep 1
+echo "\033[36;1mTeam: ★Teknik Komputer dan Jaringan 1★"
+sleep 1
+echo "\033[34;1mInformasi Lebih Lanjut Kunjungi: https://youtube.com/IndonesiaSecurityLite"
+echo "\033[31;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "Mr. RM"
+sleep 1
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+sh LITESPAM.sh
+fi
+
+if
+[ $mzr = 0 ] || [ $mzr = 0 ]
+then
+echo "\033[31;1m Kaluar"
+sleep 1
+echo "\033[32;1m Kahade ulah di serius kan nya oakakaka"
+echo "\033[32;1m Babaiii"
+sleep 1
+fi
